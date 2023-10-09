31dc542ebe033f5681829a115d93aa41741edfda
Adithya2018 <adithyansraj20@gmail.com>
2023-10-09 17:23:55 +0530

    useState hook

--- hello-world/src/StateTutorial.js
+++ hello-world/src/StateTutorial.js
@@ -1,9 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 
 const StateTutorial = () => {
+    const [counter, setCounter] = useState(0);
+    const [inputValue, setInputValue] = useState("Adi");
+
+    const increment = () => {
+        setCounter(counter + 1);
+        console.log(counter);
+    }
+
+    let onTextChange = (event) => {
+        const newValue = event.target.value;
+        setInputValue(newValue);
+    }
+
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1>{counter}</h1>
+            <button onClick={increment}>Increment</button>
+            <br />
+            <br />
+            <input type="text" placeholder="enter something" onChange={onTextChange}/>
+            <br />
+            <br />
+            <b>{inputValue}</b>
         </div>
     );
 };
